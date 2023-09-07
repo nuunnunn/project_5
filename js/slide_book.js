8ab5d63230bb86933215f33df0d88687e409f298
nuunnunn <gnr9706@gmail.com>
2023-09-07 18:03:01 +0900

    2023.09.07

---
+++ js/slide_book.js
@@ -0,0 +1,9 @@
+// let td_next = document.getElementsByClassName('td_next');
+// let td_slide = document.getElementsByClassName('td_slide');
+
+// // const bSlide_effet = {};
+// // const bSlide_time = ;
+
+// td_next.onclick = function(){
+//     td_slide.animate({transform: 'translate(-100px,0)'},{duration: 1000})
+// }
